c4d96626bfc25d5309f11fda984816f1050cd8ac
Jirka Daněk <jirka.danek@jdanek.eu>
2023-08-03 15:57:41 +0200

    Editing plugin structure, adding sample configuration

--- plugins/extend/example/lang/cs.php
+++ plugins/extend/example/lang/cs.php
@@ -2,5 +2,6 @@
 
 return [
     'plugin.name' => 'Ukázkový SL8 plugin',
-    'image.title' => "Klikni na mě"
+    'plugin.description' => 'Toto je ukázkový extend plugin Example, slouží hlavně k demonstraci základní struktury pluginu. Obsahuje i jednoduchou ukázku pro základní vlastní konfigurovatelnost.',
+    'image.title' => 'Klikni na mě',
 ];
